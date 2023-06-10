a96cd9fbd21d0ea0f5edc025e9b9b6ce3fb2d70b
Nori Lacsina <toperrrrrrrr@gmail.com>
2023-06-10 18:42:08 +0800

    .

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,33 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Form, Input, Alert, Card, Container } from 'react-bootstrap';
+import { Button, Form, Input, Alert, Card, Container, Stack } from 'react-bootstrap';
 
 
 const Login = () => {
+  const [isButtonDisabled, setButtonDisabled] = useState('primary');
+  const [isAlertHidden, setAlertHidden] = useState(true);
+
+  const handleClick = () => {
+    setButtonDisabled('dark');
+    setAlertHidden(false);
+  };
+
+
   return (
-    
     <>
     <Container>
       <Card>
         <Card.Body>
+          
           <Card.Title>Login</Card.Title>
           <Form>
+          <Stack gap={3} className="col-md-5 mx-auto">
             <input type='text' ></input>
-            <input type='password'></input>
-            <Button type='button'variant='primary'> login</Button>
-            <Alert variant='success'> Login Success </Alert>
+            <input type='password'></input> 
+            <Button type='button' variant={isButtonDisabled} onClick={handleClick} size='lg' >login</Button>
+            <Alert variant='success' hidden={isAlertHidden}> Login Success </Alert>
+            </Stack>  
           </Form>
         </Card.Body>
       </Card>
