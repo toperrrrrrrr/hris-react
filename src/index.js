62c0ded6a43f53a5cb89f35c720bf18e3e1c0dbd
Nori Lacsina <toperrrrrrrr@gmail.com>
2023-06-11 16:23:53 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import SignUp from './components/SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <SignUp />
 );
